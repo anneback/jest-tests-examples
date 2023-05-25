2406d74b6eda76e6157a56ab61f4fa305a7a93f1
Jesper Annebäck <jesper.anneback@hallon.se>
2023-05-25 11:28:37 +0200

    adds npm tdd. adds test Button tests

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { act, create } from 'react-test-renderer';
 
 import Button from '@/components/Button/Button';
 
 describe('Button', () => {
   it('should render with custom buttonText', () => {
-    <Button>Text</Button>;
+    const component = create(<Button>SomeText</Button>);
+    const button = component.root.findByType('button').props;
+    expect(button.children).toEqual('SomeText');
+  });
+
+  it('should onClick has been invoked', () => {
+    const onClickMock = jest.fn();
+
+    const component = create(<Button onClick={onClickMock}>Text</Button>);
+
+    const button = component.root.findByType('button').props;
+
+    expect(onClickMock).not.toHaveBeenCalled();
+
+    act(() => {
+      button.onClick();
+    });
+
+    expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,8 +6,8 @@ type ButtonProps = {
 };
 
 const Button = (props: ButtonProps) => {
-  const { onClick, children } = props;
-  return <button>{children}</button>;
+  const { onClick = () => {}, children } = props;
+  return <button onClick={onClick}>{children}</button>;
 };
 
 export default Button;
